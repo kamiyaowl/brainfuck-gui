55c9a05ca66b6377f9e61867afa8a1a476a025b5
Fumiya Katoh <katoh.numazu.ct@gmail.com>
2014-04-21 17:00:02 +0900

    Initial Commit

---
+++ brainfuck-gui/BrainfuckExtension.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
+using Roslyn.Scripting.CSharp;
+
+namespace Brainfuck {
+	public static class BrainfuckExtension {
+		public static bool Brainfuck(this string src, out string code, out string result) {
+			var sb = new StringBuilder();
+			sb.AppendLine(@"var ptr = 0;");
+			sb.AppendLine(@"var buffer = new char[30000];");
+			sb.AppendLine(@"var sb = new StringBuilder();");
+			foreach (var c in src) {
+				switch (c) {
+					case '>':
+						sb.AppendLine(@"++ptr;");
+						break;
+					case '<':
+						sb.AppendLine(@"--ptr;");
+						break;
+					case '+':
+						sb.AppendLine(@"buffer[ptr] = (char)(buffer[ptr] + 1);");
+						break;
+					case '-':
+						sb.AppendLine(@"buffer[ptr] = (char)(buffer[ptr] - 1);");
+						break;
+					case '.':
+						sb.AppendLine(@"sb.Append(buffer[ptr]);");
+						break;
+					case ',':
+						//not supported
+						//sb.AppendLine(@"buffer[ptr] = (char)Console.Read();");
+						break;
+					case '[':
+						sb.AppendLine(@"while(buffer[ptr] != 0) {");
+						break;
+					case ']':
+						sb.AppendLine(@"}");
+						break;
+					default:
+						break;
+				}
+			}
+			sb.AppendLine(@"sb.ToString();");
+
+			code = sb.ToString();
+			//run 
+			var engine = new ScriptEngine();
+			engine.ImportNamespace("System");
+			engine.ImportNamespace("System.Text");
+			var session = engine.CreateSession();
+			try {
+				result = session.Execute<string>(code);
+			} catch(Exception ex) {
+				result = ex.ToString();
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ brainfuck-gui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using Brainfuck;
+using brainfuck_gui.Models;
+
+namespace brainfuck_gui.ViewModels {
+	public class MainWindowViewModel : ViewModel {
+
+
+		#region FuckCode変更通知プロパティ
+		private string _FuckCode;
+
+		public string FuckCode {
+			get { return _FuckCode; }
+			set { 
+				if (_FuckCode == value)
+					return;
+				_FuckCode = value;
+				RaisePropertyChanged("FuckCode");
+				ExecuteCommand.RaiseCanExecuteChanged();
+			}
+		}
+		#endregion
+
+
+		#region RunnableCode変更通知プロパティ
+		private string _RunnableCode;
+
+		public string RunnableCode {
+			get { return _RunnableCode; }
+			set { 
+				if (_RunnableCode == value)
+					return;
+				_RunnableCode = value;
+				RaisePropertyChanged("RunnableCode");
+			}
+		}
+		#endregion
+
+
+		#region Result変更通知プロパティ
+		private string _Result;
+
+		public string Result {
+			get { return _Result; }
+			set { 
+				if (_Result == value)
+					return;
+				_Result = value;
+				RaisePropertyChanged("Result");
+			}
+		}
+		#endregion
+
+
+		#region ExecuteCommand
+		private ViewModelCommand _ExecuteCommand;
+
+		public ViewModelCommand ExecuteCommand {
+			get {
+				if (_ExecuteCommand == null) {
+					_ExecuteCommand = new ViewModelCommand(Execute, CanExecute);
+				}
+				return _ExecuteCommand;
+			}
+		}
+
+		public bool CanExecute() {
+			return !string.IsNullOrWhiteSpace(FuckCode);
+		}
+
+		public void Execute() {
+			string code, result;
+			FuckCode.Brainfuck(out code, out result);
+			RunnableCode = code;
+			Result = result;
+		}
+		#endregion
+
+		public void Initialize() {
+			FuckCode = @">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++.";
+		}
+	}
+}
